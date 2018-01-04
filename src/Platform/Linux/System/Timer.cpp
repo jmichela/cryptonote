3516db10256b94e57b4c3a1704aab8c1530e3de7
Daniel Han <daniel88han@gmail.com>
2018-01-04 04:15:45 +0000

    Fixing errors for gcc7

--- src/Platform/Linux/System/Timer.cpp
+++ src/Platform/Linux/System/Timer.cpp
@@ -89,9 +89,11 @@ void Timer::sleep(std::chrono::nanoseconds duration) {
         if (!timerContext->interrupted) {
           uint64_t value = 0;
           if(::read(timer, &value, sizeof value) == -1 ){
-            if(errno == EAGAIN || errno == EWOULDBLOCK) {
-              timerContext->interrupted = true;
-              dispatcher->pushContext(timerContext->context);
+            if(errno != EAGAIN) {
+              if(errno != EWOULDBLOCK) {
+                timerContext->interrupted = true;
+                dispatcher->pushContext(timerContext->context);
+               }
             } else {
               throw std::runtime_error("Timer::interrupt, read failed, "  + lastErrorMessage());
             }
